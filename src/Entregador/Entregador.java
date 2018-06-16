d3e4f322f2da82ed2a41ca6f1b88ce79dbca6957
Felipe <felipe@localhost.localdomain>
2018-06-16 18:35:52 -0300

    adicionado novos metodos a classe Entregador

--- src/Entregador/Entregador.java
+++ src/Entregador/Entregador.java
@@ -7,7 +7,8 @@ package Entregador;
 
 import framework.Entidade;
 import framework.Estado;
-
+import java.util.ArrayList;
+import Restaurante.Pedido;
 /**
  *
  * @author lucas
@@ -16,6 +17,8 @@ public class Entregador extends Entidade {
     public Estado idle;
     public Estado ativo;
     public int id;
+    public ArrayList<Pedido> mochila; // pedidos que ainda não foram entregues
+    public ArrayList<Pedido> pedidos_de_entrega; // buffer requisição de entregas vindas do sistema
     
     public Entregador(int lPort){
         super(lPort);
@@ -24,5 +27,32 @@ public class Entregador extends Entidade {
         ativo = new EntregadorAtivo(this);
         
         mudaEstado(idle);
+    }
+    
+    private Boolean aceitaPedido(Pedido p){
+        //envia mensagem para o sistema dizendo que quer aceitar o pedido
+        //se conseguir aceitar o pedido retorna true
+        return false;
+    }
+    
+    private void RecebePedidoDeEntrega(String pacote_pedido){
+        //desempacota(pacote_pedido);
+        int PortaRestaurante = 0;
+        int idPedido = 0;
+        
+        // aqui deve setar a PortadoRestaurante e o Id do pedido desempacotado
+        
+        Pedido p = new Pedido(PortaRestaurante, idPedido);
+        pedidos_de_entrega.add(p);
+    }
+    
+    private void SinalizaEntregaDePedido(Pedido p){
+        int PortaRestaurante = p.portaRestaurante;
+        int IdPedido = p.idPedido;
+        //envia mensagem de resposta de restaurante
+    }
+    
+    private void LimpaBufferDePedidosDeEntrega(){
+        pedidos_de_entrega.clear();
     }
 }
--- src/Restaurante/Pedido.java
+++ src/Restaurante/Pedido.java
@@ -19,4 +19,8 @@ public class Pedido {
         this.portaRestaurante = portaRestaurante;
         this.idPedido = idPedido;
     }
+
+    public Pedido() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
